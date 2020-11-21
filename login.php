16c1d61624a7519b5667e781dc14a97663298e55
Mario1620 <66508401+Mario1620@users.noreply.github.com>
2020-11-21 13:07:25 -0600

    Add files via upload

--- login.php
+++ login.php
@@ -19,23 +19,19 @@ if($row == 0){ //user does not exist
     echo "User does not exist";
 }
 else if($row == 1){
-    echo "User exists";
+    
 
     if($password == $pass && strpos($role, "ADMIN") !== false){   //user is an admin, redirect to index page  
-        echo "<br>";
-        echo "Oh Fun";
         $_SESSION['role'] = $role;
         $_SESSION['user'] = $username;
-        header("location: index.html");
+        header("location: index.php");
     }
     else if($pass != $password) //user password is wrong 
     {
         echo "wrong password or username";
 
     }
-    else{ //user is not an admin, redirect to error page
-        echo "user is not an Admin";
-    }
+    
 }
 
 ?>
