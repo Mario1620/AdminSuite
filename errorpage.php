f16434de6f2bbe69a6a2e5ff53eea96960904a75
ansalaz08 <70624047+ansalaz08@users.noreply.github.com>
2020-11-23 17:17:48 -0600

    Add files via upload

---
+++ errorpage.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <PHP> <link rel="stylesheet" type="text/css" href="errorstyle.css"></head> <PHP>
+  <title>Document</title>
+</head>
+<body>
+  <div class="container">
+    <header></header>
+    <h1>Access Denied</h1>
+    <p>You don't have permission to access on this portal.
+     Please contact your System Administrator. </p>
+    <p>Reference #<?php echo uniqid(); ?></p>
+    <footer id="footer"></footer>
+  </div>
+</body>
+</html>
